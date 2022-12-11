e9389e2e8d62d714a5140aef188df530d0b988a5
Marissa Dilig <hello.maripd@gmail.com>
2022-12-11 01:15:32 -0600

    delete API call complete, popup box for delete item complete

--- client/src/pages/JobDetails.js
+++ client/src/pages/JobDetails.js
@@ -10,6 +10,7 @@ const JobDetails = (props) => {
   const navigate = useNavigate()
   let { id } = useParams()
   const [currentData, setData] = useState([])
+  const [currentDisplay, setDisplay] = useState(false)
 
   console.log(`This is JOBID`, id)
   useEffect(() => {
@@ -32,23 +33,48 @@ const JobDetails = (props) => {
     navigate('/')
   }
 
+  const deletebtnHandleClick = () => {
+    if (currentDisplay === false) {
+      setDisplay(true)
+    }
+  }
+  let panelDisplay = 'hide'
+  if (currentDisplay === true) {
+    panelDisplay = ''
+  }
+
+  const xHandleClick = (e) => {
+    if (currentDisplay === true) {
+      setDisplay(false)
+    }
+    console.log('This X is clicked', e.target.value)
+  }
+
+  const realDeleteHandleClick = async (e) => {
+    const response = await axios.delete(`http://localhost:3001/deletejob/${id}`)
+    navigate('/')
+  }
   return (
     <div>
       <header>Mari</header>
       <div className="main-container">
         <div className="jobdetails-container">
           <section>
-            <div className="popup-box">
-              <p className="x-close"> X</p>
+            <div className={`popup-box ${panelDisplay}`}>
+              <p onClick={xHandleClick} className="x-close">
+                X
+              </p>
               <p className="popup-text">
                 Are you sure you want to remove this item?
               </p>
-              <p className="removebtn" onClick={deleteHandleClick}>
+              <p className="removebtn" onClick={realDeleteHandleClick}>
                 Delete
               </p>
             </div>
             <div className="editdelete-btns">
-              <span className="deletebtn">delete</span>
+              <span onClick={deletebtnHandleClick} className="deletebtn">
+                delete
+              </span>
               <span onClick={editHandleClick} className="editbtn">
                 edit
               </span>
